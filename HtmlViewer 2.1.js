7889aaf0947daffcbf9f5413107faf57648edace
RuizeSun <ntcgroup@qq.com>
2025-03-02 12:06:21 +0800

    2.1.4

--- HtmlViewer 2.1.js
+++ HtmlViewer 2.1.js
@@ -1,17 +1,14 @@
-console.log("成功加载 HtmlViewer 2.0。\n作者：Ruize Sun\n感谢使用！");
+console.log("成功加载 HtmlViewer 2.1.4。\n作者：Ruize Sun\n感谢使用！");
+
 // HtmlViewer 窗口初始化
 let styles = document.createElement("style");
-styles.innerHTML =
-	".HtmlViewer{position:absolute;width:720px;height:640px;z-index:9999;top:4px;right:4px;overflow:hidden;background-color:white;border:2px solid black;border-radius:6px;animation: fadein 0.15s;}#HtmlViewerCloseButton{text-decoration:none;color:red;font-size:1.5rem;cursor: pointer;}.HtmlViewerWindowTitle{background:linear-gradient(to bottom,#ccc,#999);padding: 4px;cursor: default;user-select: none;}.HtmlViewerWindowTitle > span{color:white;font-weight: bold;}.HtmlViewerBox{padding:6px;}#HtmlViewerCloseButton:hover{color:#E00}@keyframes fadein{0%{opacity:0;transform:translateY(100px);}100%{opacity:1;transform:translateY(0px);}}.HtmlViewerProjectIframeBox{height:100%;width:100%;overflow:hidden;}";
-
+styles.innerHTML = ".HtmlViewer{position:absolute;width:720px;height:640px;z-index:9999;top:4px;right:4px;overflow:hidden;background-color:white;border:2px solid black;border-radius:6px;animation: fadein 0.15s;}#HtmlViewerCloseButton{text-decoration:none;color:red;font-size:1.5rem;cursor: pointer;}.HtmlViewerWindowTitle{background:linear-gradient(to bottom,#ccc,#999);padding: 4px;cursor: default;user-select: none;}.HtmlViewerWindowTitle > span{color:white;font-weight: bold;}.HtmlViewerBox{padding:6px;}#HtmlViewerCloseButton:hover{color:#E00}@keyframes fadein{0%{opacity:0;transform:translateY(100px);}100%{opacity:1;transform:translateY(0px);}}.HtmlViewerProjectIframeBox{height:100%;width:100%;overflow:hidden;}";
 document.head.appendChild(styles);
-
 div = document.createElement("div");
 div.classList.add("HtmlViewer");
 div.classList.add("HtmlViewerWindow");
 div.setAttribute("id", "HtmlViewerWindow");
-div.innerHTML =
-	'<div class="HtmlViewerWindowTitle" id="HtmlViewerWindowTitle"><a href=\'javascript:document.body.removeChild(document.getElementById("HtmlViewerWindow"));\' id=\'HtmlViewerCloseButton\'>●</a>&nbsp;&nbsp;<span>HtmlViewer</span></div><div class="HtmlViewerBox"></div>';
+div.innerHTML = '<div class="HtmlViewerWindowTitle" id="HtmlViewerWindowTitle"><a href=\'javascript:document.body.removeChild(document.getElementById("HtmlViewerWindow"));\' id=\'HtmlViewerCloseButton\'>●</a>&nbsp;&nbsp;<span class="HVTitle">HtmlViewer</span></div><div class="HtmlViewerBox"></div>';
 async function windowMovingEvent() {
 	var e = 0,
 		o = 0,
@@ -21,19 +18,13 @@ async function windowMovingEvent() {
 	}
 	t("HtmlViewerWindowTitle").addEventListener("mousedown", function (m) {
 		m = m || window.event;
-		(e = m.pageX - t("HtmlViewerWindow").offsetLeft),
-			(o = m.pageY - t("HtmlViewerWindow").offsetTop),
-			(n = !0);
+		(e = m.pageX - t("HtmlViewerWindow").offsetLeft), (o = m.pageY - t("HtmlViewerWindow").offsetTop), (n = !0);
 	}),
 		(document.onmousemove = function (m) {
 			m = m || window.event;
 			var u = 0,
 				d = 0;
-			!0 === n &&
-				((u = m.pageX - e),
-				(d = m.pageY - o),
-				(t("HtmlViewerWindow").style.left = u + "px"),
-				(t("HtmlViewerWindow").style.top = d + "px"));
+			!0 === n && ((u = m.pageX - e), (d = m.pageY - o), (t("HtmlViewerWindow").style.left = u + "px"), (t("HtmlViewerWindow").style.top = d + "px"));
 		}),
 		(document.onmouseup = function () {
 			n = !1;
@@ -86,11 +77,15 @@ class HtmlViewer {
 				{
 					opcode: "showHtml",
 					blockType: Scratch.BlockType.COMMAND,
-					text: "显示HTML代码 [html]",
+					text: "显示HTML代码 [title] [html]",
 					arguments: {
+						title: {
+							type: Scratch.ArgumentType.STRING,
+							defaultValue: "窗口标题",
+						},
 						html: {
 							type: Scratch.ArgumentType.STRING,
-							defaultValue: "<p>Hello, World!</p>",
+							defaultValue: "窗口内容",
 						},
 					},
 				},
@@ -187,6 +182,7 @@ class HtmlViewer {
 	showHtml(args) {
 		const html = markdownToHtml(args.html);
 		div.getElementsByClassName("HtmlViewerBox")[0].innerHTML = html;
+		div.getElementsByClassName("HVTitle")[0].innerHTML = args.title;
 		document.body.appendChild(div);
 		windowMovingEvent();
 	}
@@ -194,33 +190,21 @@ class HtmlViewer {
 		if (isNaN(Number(args.projectid, 10))) {
 			console.error("作品ID不合法");
 		} else {
-			window.open(
-				"https://www.40code.com/#page=work&id=" + args.projectid,
-				"_blank",
-				"top=4,right=4,toolbar=no,menubar=no,location=no, status=no"
-			);
+			window.open("https://www.40code.com/#page=work&id=" + args.projectid, "_blank", "top=4,right=4,toolbar=no,menubar=no,location=no, status=no");
 		}
 	}
 	showUser(args, util) {
 		if (isNaN(Number(args.usrid, 10))) {
 			console.error("用户ID不合法");
 		} else {
-			window.open(
-				"https://www.40code.com/#page=user&id=" + args.usrid,
-				"_blank",
-				"top=4,right=4,toolbar=no,menubar=no,location=no, status=no"
-			);
+			window.open("https://www.40code.com/#page=user&id=" + args.usrid, "_blank", "top=4,right=4,toolbar=no,menubar=no,location=no, status=no");
 		}
 	}
 	showForum(args, util) {
 		if (isNaN(Number(args.postid, 10))) {
 			console.error("帖子ID不合法");
 		} else {
-			window.open(
-				"https://www.40code.com/#page=post&id=" + args.postid,
-				"_blank",
-				"top=4,right=4,toolbar=no,menubar=no,location=no, status=no"
-			);
+			window.open("https://www.40code.com/#page=post&id=" + args.postid, "_blank", "top=4,right=4,toolbar=no,menubar=no,location=no, status=no");
 		}
 	}
 	closeHtmlViewer(args) {
@@ -228,27 +212,11 @@ class HtmlViewer {
 	}
 	gogithub(args) {
 		if (args.type == "res") {
-			const uri = args.id
-				.replaceAll("%", "_")
-				.replaceAll("\\", "_")
-				.replaceAll("*", "_")
-				.replaceAll("..", "_");
-			window.open(
-				"https://www.github.com/" + uri,
-				"_blank",
-				"top=4,right=4,toolbar=no,menubar=no,location=no, status=no"
-			);
+			const uri = args.id.replaceAll("%", "_").replaceAll("\\", "_").replaceAll("*", "_").replaceAll("..", "_");
+			window.open("https://www.github.com/" + uri, "_blank", "top=4,right=4,toolbar=no,menubar=no,location=no, status=no");
 		} else {
-			const uri = args.id
-				.replaceAll("%", "_")
-				.replaceAll("\\", "_")
-				.replaceAll("*", "_")
-				.replaceAll("..", "_");
-			window.open(
-				"https://www.github.com/" + uri + "/",
-				"_blank",
-				"toolbar=no,menubar=no,location=no, status=no"
-			);
+			const uri = args.id.replaceAll("%", "_").replaceAll("\\", "_").replaceAll("*", "_").replaceAll("..", "_");
+			window.open("https://www.github.com/" + uri + "/", "_blank", "toolbar=no,menubar=no,location=no, status=no");
 		}
 	}
 }
