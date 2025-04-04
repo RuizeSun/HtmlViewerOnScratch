db1745f8725d8876722831bea0548100ec423a25
RuizeSun <ntcgroup@qq.com>
2025-04-04 23:21:08 +0800

    2.1.6更新
    + 现在可以自定义窗口 CSS 样式，亦可以选择一个预设的样式（待添加更多预设样式）。
    + 优化了代码结构。
    + 修改了一些功能的实现方式。

--- HtmlViewer 2.1.js
+++ HtmlViewer 2.1.js
@@ -1,15 +1,50 @@
-console.log("成功加载 HtmlViewer 2.1.5。\n作者：Ruize Sun\n感谢使用！");
+console.log("正在加载 HtmlViewer 2.1.6。\n作者：Ruize Sun\n感谢使用！");
 
-// HtmlViewer 窗口初始化
-let styles = document.createElement("style");
-styles.innerHTML = ".HtmlViewer{position:absolute;z-index:9999;top:4px;right:4px;overflow:hidden;background-color:white;border:2px solid black;border-radius:6px;animation: fadein 0.15s;}#HtmlViewerCloseButton{text-decoration:none;color:red;font-size:1.5rem;cursor: pointer;}.HtmlViewerWindowTitle{background:linear-gradient(to bottom,#ccc,#999);padding: 4px;cursor: default;user-select: none;}.HtmlViewerWindowTitle > span{color:white;font-weight: bold;}.HtmlViewerBox{padding:6px;}#HtmlViewerCloseButton:hover{color:#E00}@keyframes fadein{0%{opacity:0;transform:translateY(100px);}100%{opacity:1;transform:translateY(0px);}}.HtmlViewerProjectIframeBox{height:100%;width:100%;overflow:hidden;}";
-document.head.appendChild(styles);
-div = document.createElement("div");
-div.classList.add("HtmlViewer");
-div.classList.add("HtmlViewerWindow");
-div.setAttribute("id", "HtmlViewerWindow");
-div.innerHTML = '<div class="HtmlViewerWindowTitle" id="HtmlViewerWindowTitle"><a href=\'javascript:document.body.removeChild(document.getElementById("HtmlViewerWindow"));\' id=\'HtmlViewerCloseButton\'>●</a>&nbsp;&nbsp;<span class="HVTitle">HtmlViewer</span></div><div class="HtmlViewerBox"></div>';
+function vboxStyleInit() {
+	/*
+	【作用】窗口样式初始化
+	【原理】这个函数会定义 div#HtmlViewerStyle 在 head 标签中。
+	*/
+	let styles = document.createElement("style");
+	styles.innerHTML = ".HtmlViewer{position:absolute;z-index:9999;top:4px;right:4px;overflow:hidden;background-color:white;border:2px solid black;border-radius:6px;animation: fadein 0.15s;}#HtmlViewerCloseButton{text-decoration:none;color:red;font-size:1.5rem;cursor: pointer;}.HtmlViewerWindowTitle{background:linear-gradient(to bottom,#ccc,#999);padding: 4px;cursor: default;user-select: none;}.HtmlViewerWindowTitle > span{color:white;font-weight: bold;}.HtmlViewerBox{padding:6px;}#HtmlViewerCloseButton:hover{color:#E00}@keyframes fadein{0%{opacity:0;transform:translateY(100px);}100%{opacity:1;transform:translateY(0px);}}.HtmlViewerProjectIframeBox{height:100%;width:100%;overflow:hidden;}"; // 默认样式
+	styles.setAttribute("id", "HtmlViewerStyle");
+	document.head.appendChild(styles);
+	let sizestyles = document.createElement("style");
+	sizestyles.innerHTML = ".HtmlViewer{width: 640px;height: 480px;}";
+	sizestyles.setAttribute("id", "HtmlViewerSizeStyle");
+	document.head.appendChild(sizestyles);
+}
+
+function vboxInit() {
+	/*
+	【作用】窗口样式初始化
+	【原理】这个函数会定义 div#HtmlViewerStyle 在 head 标签中。
+	*/
+	div = document.createElement("div");
+	div.classList.add("HtmlViewer");
+	div.classList.add("HtmlViewerWindow");
+	div.setAttribute("id", "HtmlViewerWindow");
+	div.innerHTML = '<div class="HtmlViewerWindowTitle" id="HtmlViewerWindowTitle"><a href=\'javascript:document.body.removeChild(document.getElementById("HtmlViewerWindow"));\' id=\'HtmlViewerCloseButton\'>●</a>&nbsp;&nbsp;<span class="HVTitle">HtmlViewer</span></div><div class="HtmlViewerBox"></div>';
+}
+
+function markdownToHtmlS(md) {
+	/*
+	【作用】Markdown 带过滤转换 HTML 
+	【原理】检测 markdownToHtml 是否被定义，如果没有就警告并输出原文，如果有就调用。
+	*/
+	if (typeof markdownToHtml !== "undefined") {
+		return markdownToHtml(md);
+	} else {
+		console.warn("非 40code 社区，无法使用安全过滤及 Markdown 渲染。");
+		return md;
+	}
+}
+
 async function windowMovingEvent() {
+	/*
+	【作用】窗口移动事件
+	【原理】检测窗口移动事件，并调整位置。
+	*/
 	var e = 0,
 		o = 0,
 		n = !1;
@@ -30,36 +65,10 @@ async function windowMovingEvent() {
 			n = !1;
 		});
 }
-// 作品展示UI更改
-function changeButton() {
-	/**
-	document
-		.getElementById("HtmlViewerIframe")
-		.getElementsByClassName(
-			"ma-2 sd v-btn v-btn--is-elevated v-btn--has-bg theme--light v-size--default accent"
-		)[0].href = "https://www.40code.com/#page=work&id=" + args.projectid;
-	document
-		.getElementById("HtmlViewerIframe")
-		.getElementsByClassName(
-			"ma-2 sd v-btn v-btn--is-elevated v-btn--has-bg theme--light v-size--default accent"
-		)[0]
-		.getElementById("HtmlViewerIframe")
-		.getElementsByClassName("v-btn__content")[0].innerHTML = "重新加载作品";
-	document
-		.getElementById("HtmlViewerIframe")
-		.getElementsByClassName(
-			"v-icon notranslate mdi mdi-search theme--light"
-		)[0]
-		.parentNode.parentNode.setAttribute("disabled", "true");
-	document
-		.getElementById("HtmlViewerIframe")
-		.getElementsByClassName(
-			"v-icon notranslate mdi mdi-search theme--light"
-		)[0]
-		.parentNode.setAttribute("style", "cursor: no-drop;");*/
-}
-// Turbo 平台支持
-// 主 Class
+
+/*      主 Class     */
+vboxStyleInit();
+vboxInit();
 class HtmlViewer {
 	constructor(runtime) {
 		this.runtime = runtime;
@@ -72,20 +81,47 @@ class HtmlViewer {
 			blocks: [
 				{
 					blockType: Scratch.BlockType.LABEL,
+					text: "窗口样式设置",
+				},
+				{
+					opcode: "setPresetStyle",
+					blockType: Scratch.BlockType.COMMAND,
+					text: "设置预设窗口样式为 [preset]",
+					arguments: {
+						preset: {
+							type: Scratch.ArgumentType.STRING,
+							defaultValue: "default",
+							menu: "presetstyle",
+						},
+					},
+				},
+				{
+					opcode: "setStyle",
+					blockType: Scratch.BlockType.COMMAND,
+					text: "自定义窗口样式为 [style]",
+					arguments: {
+						style: {
+							type: Scratch.ArgumentType.STRING,
+							defaultValue: "用法见 Github Wiki",
+						},
+					},
+				},
+				{
+					blockType: Scratch.BlockType.LABEL,
 					text: "基本功能",
 				},
 				{
 					opcode: "showHtml",
 					blockType: Scratch.BlockType.COMMAND,
-					text: "显示HTML代码。[title] [html] 【大小：[width]×[height]（单位：px）】",
+					text: "显示大小为 [width] × [height] 标题为 [title] 内容为 [html] 的窗口（单位：px）",
 					arguments: {
 						title: {
 							type: Scratch.ArgumentType.STRING,
-							defaultValue: "窗口标题",
+							defaultValue: "Hello, world!",
 						},
 						html: {
 							type: Scratch.ArgumentType.STRING,
-							defaultValue: "窗口内容",
+							defaultValue: "你好，世界！",
 						},
 						width: {
 							type: Scratch.ArgumentType.STRING,
@@ -183,15 +219,34 @@ class HtmlViewer {
 						},
 					],
 				},
+				presetstyle: {
+					acceptReporters: false,
+					items: [
+						{
+							text: "默认样式",
+							value: "default",
+						},
+					],
+				},
 			},
 		};
 	}
 
+	setPresetStyle(args) {
+		let presets = { default: '".HtmlViewer{position:absolute;z-index:9999;top:4px;right:4px;overflow:hidden;background-color:white;border:2px solid black;border-radius:6px;animation: fadein 0.15s;}#HtmlViewerCloseButton{text-decoration:none;color:red;font-size:1.5rem;cursor: pointer;}.HtmlViewerWindowTitle{background:linear-gradient(to bottom,#ccc,#999);padding: 4px;cursor: default;user-select: none;}.HtmlViewerWindowTitle > span{color:white;font-weight: bold;}.HtmlViewerBox{padding:6px;}#HtmlViewerCloseButton:hover{color:#E00}@keyframes fadein{0%{opacity:0;transform:translateY(100px);}100%{opacity:1;transform:translateY(0px);}}.HtmlViewerProjectIframeBox{height:100%;width:100%;overflow:hidden;}"' };
+		let styles = document.getElementById("HtmlViewerStyle");
+		styles.innerHTML = presets[args.preset];
+	}
+
+	setStyle(args) {
+		let styles = document.getElementById("HtmlViewerStyle");
+		styles.innerHTML = args.style.replaceAll("<", "").replaceAll(">", "");
+	}
+
 	showHtml(args) {
-		const html = markdownToHtml(args.html);
-		let styles = document.createElement("style");
+		const html = markdownToHtmlS(args.html);
+		let styles = document.getElementById("HtmlViewerSizeStyle");
 		styles.innerHTML = ".HtmlViewer{width: " + args.width + "px;height: " + args.height + "px;}";
-		document.head.appendChild(styles);
 		div.getElementsByClassName("HtmlViewerBox")[0].innerHTML = html;
 		div.getElementsByClassName("HVTitle")[0].innerHTML = args.title;
 		document.body.appendChild(div);
